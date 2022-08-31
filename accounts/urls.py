21102bea83a541a3da6932fc302930c0302fa0b4
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2022-08-31 22:45:04 +0530

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',views.home_view,name="home_view"),
+    path('recipe/',views.recipe_page_view,name="recipe_page_view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
 
     return render(request,'home.html')
+
+def recipe_page_view(request):
+
+    return render(request, 'recipe_page.html')
